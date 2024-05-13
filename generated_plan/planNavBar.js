90880ddbf758a558f70581508a9452c89d8e8143
Suee18 <121303661+Suee18@users.noreply.github.com>
2024-05-14 00:33:28 +0300

    plan all done

--- generated_plan/planNavBar.js
+++ generated_plan/planNavBar.js
@@ -1,15 +1,14 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
     //path if inside a folder 
     function getNavbarPath() {
-        return 'hotel.html';
+
+        return '/generated_plan/planNavBar.html';
     }
 
     fetch(getNavbarPath())
         .then(response => response.text())
         .then(data => {
-            document.getElementById('hotels_call').innerHTML = data;
+            document.getElementById('planNavBar_call').innerHTML = data;
         })
         .catch(error => {
             console.error('Error fetching navbar:', error);
